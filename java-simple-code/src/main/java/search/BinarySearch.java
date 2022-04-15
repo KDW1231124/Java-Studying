6c8553732e159b6c4303e21283553ac2b521867f
KDW1231124 <area153@naver.com>
2022-04-15 12:03:32 +0900

    java studying

--- java-simple-code/src/main/java/search/BinarySearch.java
+++ java-simple-code/src/main/java/search/BinarySearch.java
@@ -1,37 +1,38 @@
 package search;
 
 public class BinarySearch {
-    public static boolean iterator(int [] arr, int target) {
-        int low = 0;
-        int high = arr.length - 1;
-        int mid;
+  public static boolean iterator(int [] arr, int target) {
+    int low = 0;
+    int high = arr.length - 1;
+    int mid;
 
-        while (low <= high) {
-            mid = (low + high) / 2;
-            if (arr[mid] < target) {
-                low = mid + 1;
-            } else if (arr[mid] > target) {
-                high = mid - 1;
-            } else {
-                return true;
-            }
-        }
-
-        return false;
+    while (low <= high) {
+      mid = (low + high) / 2;
+      if (arr[mid] < target) {
+        low = mid + 1;
+      } else if (arr[mid] > target) {
+        high = mid - 1;
+      } else {
+        return true;
+      }
     }
 
-    public static boolean recursive(int[] arr, int target, int low, int high) {
-        if (low > high) {
-            return false;
-        }
+    return false;
+  }
 
-        int mid = (low + high) / 2;
-        if (arr[mid] < target) {
-            return recursive(arr, target, mid + 1, high);
-        } else if (arr[mid] > target) {
-            return recursive(arr, target, low, mid - 1);
-        } else {
-            return true;
-        }
+  public static boolean recursive(int[] arr, int target, int low, int high) {
+    if (low > high) {
+      return false;
     }
+
+    int mid = (low + high) / 2;
+    if (arr[mid] < target) {
+      return recursive(arr, target, mid + 1, high);
+    } else if (arr[mid] > target) {
+      return recursive(arr, target, low, mid - 1);
+    } else {
+      return true;
+    }
+  }
 }
+//
